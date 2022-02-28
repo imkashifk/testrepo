c99b76a03464158bd238f6c5051532ffe0808eb6
Kashif Khan <imkashifk@gmail.com>
2022-02-28 22:56:07 +0530

    Commited using Deesktop Lab

--- firstpython.py
+++ firstpython.py
@@ -2,3 +2,5 @@
 print("New Python File")
 #Added a new line through Lab
 print("This line is added through Lab")
+#Added a new line through Desktop Lab
+print("This line is added through Desktop Lab")
